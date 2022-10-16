e674bcec75f8280049be469551e47340b16f3879
kangdongil <akang8150@naver.com>
2022-10-16 10:53:04 +0000

    bookingAPI

--- rooms/views.py
+++ rooms/views.py
@@ -1,15 +1,19 @@
 from django.db import transaction
+from django.utils import timezone
+
 from rest_framework.views import APIView
 from rest_framework.status import HTTP_204_NO_CONTENT
 from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 from rest_framework.response import Response
 from rest_framework.exceptions import NotFound, NotAuthenticated, ParseError, PermissionDenied
-from common.paginations import ListPagination
-from categories.models import Category
 from .models import Room, Amenity
 from .serializers import AmenitySerializer, RoomListSerializer, RoomDetailSerializer
+from common.paginations import ListPagination
+from categories.models import Category
+from bookings.models import Booking
 from reviews.serializers import ReviewSerializer
 from medias.serializers import PhotoSerializer
+from bookings.serializers import PublicBookingSerializer
 
 class AmenityList(APIView, ListPagination):
     
@@ -254,4 +258,24 @@ class RoomPhotos(APIView, ListPagination):
             return Response(serializer.data)
         else:
             return Response(serializer.errors)
-        
+
+class RoomBookings(APIView):
+    
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    
+    def get_object(self, pk):
+        try:
+            return Room.objects.get(pk=pk)
+        except Room.DoesNotExist:
+            return NotFound
+    
+    def get(self, request, pk):
+        room = self.get_object(pk)
+        now = timezone.localtime(timezone.now()).date()
+        bookings = Booking.objects.filter(
+            room=room,
+            kind=Booking.BookingKindChoices.ROOM,
+            check_in__gt=now,
+        )
+        serializer = PublicBookingSerializer(bookings, many=True)
+        return Response(serializer.data)
