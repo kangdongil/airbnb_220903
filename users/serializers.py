cad8ba439fa264a3351079df228995bb9c988549
kangdongil <akang8150@naver.com>
2022-10-17 12:12:11 +0000

    12.0 GET UserAPI

--- users/serializers.py
+++ users/serializers.py
@@ -1,10 +1,25 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
 from .models import User
 
-class TinyUserSerializer(ModelSerializer):
+class TinyUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = (
         	"name",
             "avatar",
+        )
+        
+class PrivateUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        exclude = (
+        	"id",
+            "password",
+            "is_superuser",
+            "is_staff",
+            "is_active",
+            "first_name",
+            "last_name",
+            "groups",
+            "user_permissions",
         )
